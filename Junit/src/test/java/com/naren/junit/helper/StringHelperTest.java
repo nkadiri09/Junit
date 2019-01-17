081b00630a54691290471f366b37c778185f8403
nkadiri09 <nkadiri09@gmail.com>
2019-01-16 23:39:18 -0600

    Negative Senarios added
    assertFalse
    
    @BeforeClass
    
    Added

--- Junit/src/test/java/com/naren/junit/helper/StringHelperTest.java
+++ Junit/src/test/java/com/naren/junit/helper/StringHelperTest.java
@@ -2,20 +2,32 @@ package com.naren.junit.helper;
 
 import static org.junit.Assert.*;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class StringHelperTest {
 
+	static StringHelper sh;
+
+	@BeforeClass
+	public static void runOnceBeforeClass() {
+		System.out.println("@BeforeClass - runOnceBeforeClass");
+		sh = new StringHelper();
+	}
+
 	@Test
 	public void testTruncateAInFirst2Positions_AinFirst2Positions() {
-		StringHelper sh = new StringHelper();
 		assertEquals("CD", sh.truncateAInFirst2Positions("AACD"));
 	}
-	
+
 	@Test
 	public void testTruncateAInFirst2Positions_AinFirstPositions() {
-		StringHelper sh = new StringHelper();
 		assertEquals("CD", sh.truncateAInFirst2Positions("ACD"));
+	}
+	@Test
+	public void testAreFirstAndLastTwoCharactersTheSame_NegativeSenario() {
+		System.out.println("Printing the value: "+sh.areFirstAndLastTwoCharactersTheSame("ABDB"));
+		assertFalse(sh.areFirstAndLastTwoCharactersTheSame("ABDB"));
 	}
 
 }
