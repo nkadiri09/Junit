869c59757a095a935ec20ffe168d7124811e2b2a
nkadiri09 <nkadiri09@gmail.com>
2019-01-22 22:51:35 -0600

    Exceptions handling in JUNIT

---
+++ Junit/src/test/java/com/naren/junit/helper/Exception1Test.java
@@ -0,0 +1,31 @@
+package com.naren.junit.helper;
+
+import org.junit.Test;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Exception1Test {
+
+	@Test(expected = ArithmeticException.class)
+	public void testDivisionWithException() {
+		int i = 1 / 0;
+
+		// Negative Scenario
+
+		// int i = 1 / 2;
+	}
+
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void testEmptyList() {
+		new ArrayList<>().get(0);
+
+		// Negative Senario
+
+		/*
+		 * String[] a = {"Ramesh", "Reddy", "Kadiri","Narendra"}; List<String> names =
+		 * Arrays.asList(a);
+		 */
+	}
+
+}
