073d977d6c21c4a6cc9b513f6304f749311d3c54
nkadiri09 <nkadiri09@gmail.com>
2019-02-20 23:16:33 -0600

    TestSuite created to run set of test cases together.

---
+++ Junit/src/test/java/com/naren/junit/helper/TestSuite.java
@@ -0,0 +1,13 @@
+package com.naren.junit.helper;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ArrayTest.class, Exception1Test.class})
+public class TestSuite {
+	
+	
+
+}
